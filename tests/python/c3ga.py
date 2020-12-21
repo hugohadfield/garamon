ba023e908c484ea3abe959957ef488e7b7043da1
hugohadfield <hadfield.hugo@gmail.com>
2020-12-21 22:09:29 +0000

    attempt to match eo between garamon and clifford

--- tests/python/c3ga.py
+++ tests/python/c3ga.py
@@ -2,6 +2,7 @@ from c3ga_py import *
 from clifford.g3c import layout as g3c
 from clifford.g3c import eo as g3c_eo
 from clifford.g3c import einf as g3c_einf
+g3c_no = 2*g3c_eo
 
 
 def test_basics():
@@ -12,7 +13,7 @@ def test_basics():
   print("mv1 garamon: ", mv1 )
   
   # Clifford version
-  mv1_clifford  = 1.0 + g3c_eo*42.0
+  mv1_clifford  = 1.0 + g3c_no*42.0
   print("mv1 clifford: ", mv1_clifford)
   
   # Garamon version
@@ -23,7 +24,7 @@ def test_basics():
   print("mv2 garamon: " , mv2 );
   
   # Clifford version
-  mv2_clifford  = 1.0 + g3c.blades['e1']*2.0 + g3c_eo*g3c.blades['e123']*g3c_einf + 2*g3c_eo*g3c.blades['e1']
+  mv2_clifford  = 1.0 + g3c.blades['e1']*2.0 + g3c_no*g3c.blades['e123']*g3c_einf + 2*g3c_no*g3c.blades['e1']
   print("mv2 clifford: ", mv2_clifford)
 
   # some products
